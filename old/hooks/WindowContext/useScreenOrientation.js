4246bc748c8e2c7ef95433ad45718800b2f237ed
intern-laptop <justin.c.kirk@gmail.com>
2022-09-27 16:23:55 -0400

    cleans debt

--- old/hooks/WindowContext/useScreenOrientation.js
+++
@@ -1,9 +0,0 @@
-import {useContext} from 'react';
-import {ScreenContext} from './ScreenContext.js';
-
-const useScreenOrientation = () => {
-  const [orientation, setOrientation] = useContext(ScreenContext);
-  return orientation;
-};
-
-export default useScreenOrientation;
--- old/hooks/WindowContext/useWindowSize.js
+++
@@ -1,9 +0,0 @@
-import {useContext} from 'react';
-import {WindowContext} from './WindowContext.js';
-
-const useWindowSize = () => {
-  const [size, setSize] = useContext(WindowContext);
-  return size;
-};
-
-export default useWindowSize;
